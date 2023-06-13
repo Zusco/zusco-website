1ec72bf019efad0d1d264079d5a15e2eaf607145
Thekodezilla <thekodezilla@gmail.com>
2023-06-13 16:59:05 +0100

    new update

--- src/utils/apiInstance.js
+++ src/utils/apiInstance.js
@@ -39,7 +39,7 @@ export function apiInstance2(
   } else {
     url = `${process.env.NEXT_PUBLIC_API_URL}${endpoint}`;
   }
-  if (!customConfig?.internal && !token) {
+  if (!customConfig?.internal && !token && !endpoint?.includes("auth/verify")) {
     return;
   }
   return axios(url, config)
